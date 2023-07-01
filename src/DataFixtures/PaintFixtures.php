1787666f84f304ec876b8361392e9323fc6ea62b
Gibon_david <davidgbn33@gmail.com>
2023-07-01 15:55:41 +0200

    add show page

--- src/DataFixtures/PaintFixtures.php
+++ src/DataFixtures/PaintFixtures.php
@@ -10,35 +10,12 @@ use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 
-const PAINT = [
-    1 => [
-        'paint_name' => 'De la vigne au vin',
-        'description' => 'Exercice sur les transparences, et quoi de mieux qu\'une bouteille
-              de vin pour s\'exercer dans la région bordelaise.',
-        'paint_image' => 'https://drive.google.com/drive/folders/1qSfY0eo8GGZYf2lm3KXxxmQU5XWehFnu',
-        'inspiration' => '',
-        'image_inspiration' => '',
-        'taille' => 'Papier 300 gr 60 x 40',
-        'category' => 'Aquarelle',
-    ],
-    2 => [
-        'paint_name' => 'Bâteau sur l\'eau',
-        'description' => 'exercice de transparence des couleurs sur le papier',
-        'paint_image' => 'https://drive.google.com/drive/folders/1qSfY0eo8GGZYf2lm3KXxxmQU5XWehFnu',
-        'inspiration' => 'les modèles d\'estampe japonaise s\'y prête bien.',
-        'image_inspiration' => '',
-        'taille' => 'Papier 300 gr 36 x26',
-        'category' => 'Aquarelle',
-    ],
-];
-
-
 class PaintFixtures extends Fixture
 {
     public const CATEGORY = [
-        'Aquarelle',
+       'Aquarelle',
         'Pastel',
-        'Peinture',
+       'Peinture',
     ];
     public const PAINT = [
         1 => [
@@ -204,7 +181,7 @@ class PaintFixtures extends Fixture
             'inspiration' => '',
             'image_inspiration' => '',
             'taille' => 'Papier 300 gr 60 x 40',
-            'category' => 'Aquarelle',
+            'category' => 'Peinture',
         ],
     ];
 
@@ -218,15 +195,7 @@ class PaintFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
 
-        $categories = [];
-        foreach (self::CATEGORY as $value) {
-            $category = new Category();
-            $category->setCategoryName($value);
-            $manager->persist($category);
 
-            $categories[] = $category;
-
-        }
         $userAdmin = new User();
         $userAdmin->setfullName('admin')
             ->setEmail('admin@me.fr')
@@ -243,6 +212,18 @@ class PaintFixtures extends Fixture
 
         $manager->persist($user);
 
+
+        $categories = [];
+        foreach (self::CATEGORY as $value) {
+            $category = new Category();
+            $category->setCategoryName($value);
+            $this->addReference('category_' . $value, $category);
+            $manager->persist($category);
+
+
+            $categories[] = $category;
+        }
+
         $paints = [];
         foreach (self::PAINT as $key => $value) {
             $paint = new Paint();
@@ -252,12 +233,11 @@ class PaintFixtures extends Fixture
             $paint->setInspiration($value['inspiration']);
             $paint->setImageInspiration($value['image_inspiration']);
             $paint->setTaille($value['taille']);
-            $paint->setCategory($category);
+            $paint->setCategory($this->getReference('category_' . $value['category']));
             $manager->persist($paint);
 
             $paints[] = $paint;
         }
-
         $manager->flush();
     }
 }
