ae619c9b5387cc094b77f9971744dedbb588d2f3
Gibon_david <davidgbn33@gmail.com>
2023-07-02 11:25:59 +0200

    add services for category

---
+++ src/service/CategoryService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\service;
+
+use App\Repository\CategoryRepository;
+
+class CategoryService
+{
+    private $categoryRepository;
+
+    public function __construct(CategoryRepository $categoryRepository)
+    {
+        $this->categoryRepository = $categoryRepository;
+    }
+
+    public function getAllCategories()
+    {
+        return $this->categoryRepository->findAll();
+    }
+}
