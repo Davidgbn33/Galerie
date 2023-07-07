36d88b2939a883bc66ae0d7e2e8a0992778e224b
Gibon_david <davidgbn33@gmail.com>
2023-07-07 14:59:44 +0200

    add form comment

---
+++ src/Form/CommentType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Comment;
+use App\Entity\Paint;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('comment', TextareaType::class, [
+                'label'=>'Votre Message',
+
+            ])
+            ->add('paint', HiddenType::class, [
+            ])
+            ->add('send', SubmitType::class, [
+                'label'=>'Envoyer'
+            ])
+        ;
+
+        $builder->get('paint')
+            ->addModelTransformer(new CallbackTransformer(
+                fn (Paint $paint) => $paint->getId(),
+                fn (Paint $paint) => $paint->getPaintName()
+            ));
+    }
+public function configureOptions(OptionsResolver $resolver)
+{
+    $resolver->setDefaults([
+        'data_class'=> Comment::class,
+        'csrf_token_id'=>'comment_add',
+
+    ]);
+}
+}
