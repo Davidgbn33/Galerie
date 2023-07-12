4898d88f0c9d6d86ed54ee94fe9789977c96fb71
Gibon_david <davidgbn33@gmail.com>
2023-07-12 05:42:29 +0200

    Update controller Admin User with delete

--- src/Controller/admin/AdminUserController.php
+++ src/Controller/admin/AdminUserController.php
@@ -4,14 +4,17 @@ namespace App\Controller\admin;
 
 use AllowDynamicProperties;
 use App\Entity\Paint;
+use App\Entity\User;
 use App\Repository\UserRepository;
 use App\service\CategoryService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-#[AllowDynamicProperties] #[Route ('/admin_user', name: 'admin_user')]
+#[AllowDynamicProperties] #[Route ('/admin_user', name: 'admin_user_')]
 class AdminUserController extends AbstractController
 {
     public function __construct(CategoryService $categoryService)
@@ -28,5 +31,21 @@ class AdminUserController extends AbstractController
             'users' => $users,
             'categories' => $categories,
         ]);
+    }
+
+    #[Route('/{id}', name: 'delete', methods: ['GET','POST'])]
+    public function delete(
+        Request $request,
+        User $user,
+        UserRepository $userRepository,
+        EntityManagerInterface $manager
+    ): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+
+            $userRepository->remove($user, true);
+        }
+
+        return $this->redirectToRoute('admin_user_list',[], Response::HTTP_SEE_OTHER);
     }
 }
